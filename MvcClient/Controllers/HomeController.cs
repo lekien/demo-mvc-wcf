998d5a6013556dc3c6e9a4d5b300e16ed15a23d4
lekien <lekien@users.noreply.github.com>
2021-10-13 17:05:07 +0700

    first commit
    first commit

---
+++ MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using MvcClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        ServiceReference1.Service1Client ur = new ServiceReference1.Service1Client();
+        public ActionResult Index()
+        {
+            List<MemberDetail> memberDetails = new List<MemberDetail>();
+            var lst = ur.GetAllUser();
+            foreach (var item in lst)
+            {
+                MemberDetail itemMember = new MemberDetail();
+                itemMember.Id = item.Id;
+                itemMember.UserName = item.UserName;
+                itemMember.Email = item.Email;
+                itemMember.Phone = item.Phone;
+                item.Address = item.Address;
+                memberDetails.Add(itemMember);
+            }
+            return View(memberDetails);
+        }
+
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(MemberDetail member)
+        {
+            ur.AddMember(member.UserName, member.Email, member.Phone, member.Address);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var memberExist = ur.GetMemberById(id);
+            MemberDetail memberItem = new MemberDetail();
+            if (memberExist != null)
+            {
+                memberItem.Id = memberExist.Id;
+                memberItem.UserName = memberExist.UserName;
+                memberItem.Email = memberExist.Email;
+                memberItem.Phone = memberExist.Phone;
+                memberItem.Address = memberExist.Address;
+            }
+            return View("Add", memberItem);
+        }
+        [HttpPost]
+        public ActionResult Edit(MemberDetail member)
+        {
+            int Retval = ur.UpdateMember(member.Id, member.UserName, member.Email, member.Phone, member.Address);
+            if (Retval > 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            int retval = ur.DeleteMemberById(id);
+            if (retval > 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WcfService/Service1.svc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int AddMember(string UserName, string Email, string Phone, string Address)
+        {
+            DemoWCFEntities _dbEntity = new DemoWCFEntities();
+            Member newMember = new Member();
+            newMember.UserName = UserName;
+            newMember.Email = Email;
+            newMember.Phone = Phone;
+            newMember.Address = Address;
+            _dbEntity.Members.Add(newMember);
+            int Retval = _dbEntity.SaveChanges();
+            return Retval;
+        }
+
+        public int DeleteMemberById(int Id)
+        {
+            DemoWCFEntities _dbEntity = new DemoWCFEntities();
+            var member = _dbEntity.Members.FirstOrDefault(p => p.Id == Id);
+            _dbEntity.Entry(member).State = EntityState.Deleted;
+
+            int Retval = _dbEntity.SaveChanges();
+            return Retval;
+        }
+
+        public List<Member> GetAllUser()
+        {
+            DemoWCFEntities _dbEntity = new DemoWCFEntities();
+            var lstMember = _dbEntity.Members.ToList();
+
+            return lstMember;
+        }
+
+        public Member GetMemberById(int Id)
+        {
+            DemoWCFEntities _dbEntity = new DemoWCFEntities();
+            var member = _dbEntity.Members.FirstOrDefault(p => p.Id == Id);
+
+            return member;
+        }
+
+        public int UpdateMember(int Id, string UserName, string Email, string Phone, string Address)
+        {
+            DemoWCFEntities _dbEntity = new DemoWCFEntities();
+            var member = _dbEntity.Members.FirstOrDefault(p => p.Id == Id);
+            member.UserName = UserName;
+            member.Email = Email;
+            member.Phone = Phone;
+            member.Address = Address;
+            _dbEntity.Entry(member).State = EntityState.Modified;
+
+            int Retval = _dbEntity.SaveChanges();
+            return Retval;
+        }
+    }
+}
